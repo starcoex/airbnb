3218d556e9826c8a1f34e7bab38cdbd197518607
starcoex <69073390+starcoex@users.noreply.github.com>
2022-11-01 10:47:39 +0900

    python, oop inheritance first

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
 
 class Human:
     def __init__(self, name):
+        print("human initialized")
         self.name = name
 
     def say_Hello(self):
         print(f"hello my name is {self.name}")
 
 
-class Player:
+class Player(Human):
     def __init__(self, name, xp):
-        self.name = name
+        super().__init__(name)
         self.xp = xp
 
-    def say_Hello(self):
-        print(f"hello my name is {self.name}")
 
-
-class Fan:
+class Fan(Human):
     def __init__(self, name, fav_team):
-        self.name = name
+        super().__init__(name)
         self.fav_team: fav_team
-
-    def say_Hello(self):
-        print(f"hello my name is {self.name}")
 
 
 nico = Player("nico", 20)
 print(nico.name)
 nico.say_Hello()
-ko = Fan("Ko", 21)
+ko = Fan("Ko", "21")
 ko.say_Hello()
